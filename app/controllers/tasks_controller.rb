3f27fe20448395d5cbff64c02b05771c749b9986
yoshito <yoshito5211@gmail.com>
2021-07-31 02:41:19 +0900

    発展的な機能の追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,34 +1,32 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[show edit update destroy]
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks.recent
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
+  def show; end
 
   def new
     @task = Task.new
   end
 
   def create
-    task = Task.new(task_params)
-    task.save!
-    redirect_to root_path, notice: "タスク「#{task.name}」を登録しました。"
+    @task = current_user.tasks.new(task_params)
+    if @task.save
+      redirect_to root_path, notice: "タスク「#{@task.name}」を登録しました。"
+    else
+      render :new
+    end
   end
 
-  def edit
-    @task = Task.find(params[:id])
-  end
+  def edit; end
 
   def update
-    task = Task.find(params[:id])
-    task.update!(task_params)
-    redirect_to root_path, notice: "タスク「#{task.name}」を更新しました。"
+    @task.update!(task_params)
+    redirect_to root_path, notice: "タスク「#{@task.name}」を更新しました。"
   end
 
   def destroy
-    task = Task.find(params[:id])
     task.destroy!
     redirect_to root_path, notice: "タスク「#{task.name}」を削除しました。"
   end
@@ -37,5 +35,9 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:name, :description)
+  end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
   end
 end
