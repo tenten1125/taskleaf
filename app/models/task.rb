407d482fac2f2182e0d94e99ae5d5f32eb044053
yoshito <yoshito5211@gmail.com>
2021-08-02 01:25:37 +0900

    タスク検索条件を名称と登録日時のみに絞る

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,12 @@ class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
   belongs_to :user
   scope :recent, -> { order(created_at: :desc) }
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[name created_at]
+  end
+
+  def self.ransackable_associations(_auth_object = nil)
+    []
+  end
 end
