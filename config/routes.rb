96e78dcb4c47daee1871b496f8d78f5a3a5e62e7
yoshito <yoshito5211@gmail.com>
2021-08-02 00:58:10 +0900

    タスク新規登録画面の後の確認画面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
     resources :users
   end
   root 'tasks#index'
-  resources :tasks
+  resources :tasks do
+    post :confirm, action: :confirm_new, on: :new
+  end
 end
